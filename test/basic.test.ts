50b0159a90ec2c6d4d45e6c2715fa2a6ba25cbaf
chenjiezi <892774940@qq.com>
2022-10-27 15:59:11 +0800

    test: require as object property

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import requireSupport from '../src/index'
 
-test('case1', async () => {
+test('variable declaration', async () => {
   const source = `
     const foo = require('foo');
     const bar = require('bar');
@@ -14,3 +14,19 @@ test('case1', async () => {
     const bar = hash_bar;"
   `)
 })
+
+test('object property', async () => {
+  const source = `
+    const obj = {
+      foo: require('foo')
+    }
+  `
+  const { code } = await requireSupport().transform(source)
+  expect(code).toMatchInlineSnapshot(`
+    "import hash_foo from \\"foo\\";
+    const obj = {
+      foo: hash_foo
+    };"
+  `)
+})
+
