02044d3771ce7e241191f0bce36fb82971014d71
chenjiezi <892774940@qq.com>
2022-10-30 14:55:24 +0800

    feat: "parseRequirePath" tool funciton

--- src/utils.ts
+++ src/utils.ts
@@ -4,9 +4,22 @@ export const isRawMethodCheck = (methodName: string): boolean => {
   return Object.prototype.toString.call([][methodName]).includes('Function')
     || Object.prototype.toString.call({}[methodName]).includes('Function')
 }
-// TODO:
+
 export const parseRequirePath = (path: string): myType.PathElement => {
+  let prefixPath = ''
+  let moduleId = ''
+  let suffix = ''
+  path.replace(/(.*\/)*([^.]+).*/ig, (match, a, b) => {
+    moduleId = b ?? ''
+    prefixPath = a ?? ''
+    return ''
+  })
+  if (prefixPath.length + moduleId.length < path.length)
+    suffix = path.slice(prefixPath.length + moduleId.length, path.length)
+
   return {
-    moduleId: path,
+    moduleId,
+    prefixPath,
+    suffix,
   }
 }
