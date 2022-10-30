9423f93b780f7f07d997b4c6e40de7a7204b600a
chenjiezi <892774940@qq.com>
2022-10-30 11:49:47 +0800

    test: wip

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -3,48 +3,52 @@ import requireSupport from '../src/index'
 
 test('variable declaration', async () => {
   const source = `
-    const foo = require('foo');
-    const bar = require('bar');
+    const foo = require('module1');
+    const bar = require('module2');
   `
   const { code } = await requireSupport().transform(source)
   expect(code).toMatchInlineSnapshot(`
-    "import hash_foo from \\"foo\\";
-    import hash_bar from \\"bar\\";
-    const foo = hash_foo;
-    const bar = hash_bar;"
+    "import hash_module1 from \\"module1\\";
+    import hash_module2 from \\"module2\\";
+    const foo = hash_module1;
+    const bar = hash_module2;"
   `)
 })
 
 test('object property', async () => {
   const source = `
     const obj = {
-      foo: require('foo')
+      foo: require('module1')
     }
   `
   const { code } = await requireSupport().transform(source)
   expect(code).toMatchInlineSnapshot(`
-    "import hash_foo from \\"foo\\";
+    "import hash_module1 from \\"module1\\";
     const obj = {
-      foo: hash_foo
+      foo: hash_module1
     };"
   `)
 })
-
+// TODO:
 test('conditional expression', async () => {
   const source = `
     const foo = true ? require('bar') : ''
   `
   const { code } = await requireSupport().transform(source)
-  expect(code).toMatchInlineSnapshot('"const foo = true ? require(\'bar\') : \'\';"')
+  expect(code).toMatchInlineSnapshot(`
+    "import hash_bar from \\"bar\\";
+    const foo = true ? require('bar') : '';"
+  `)
 })
-
+// TODO:
 test('return statement', async () => {
   const source = `
     function foo () { return require('bar') }
   `
   const { code } = await requireSupport().transform(source)
   expect(code).toMatchInlineSnapshot(`
-    "function foo() {
+    "import hash_bar from \\"bar\\";
+    function foo() {
       return require('bar');
     }"
   `)
--- test/requirePath.test.ts
+++ test/requirePath.test.ts
@@ -3,13 +3,14 @@ import requireSupport from '../src/index'
 
 test('template literal', async () => {
   // eslint-disable-next-line no-template-curly-in-string, @typescript-eslint/quotes
-  const source = "const Atl = 'a';const Btl = 'b';const bar = require(`${Atl}/x/${Btl}`)"
+  const source = "const Atl = 'a';const Btl = 'b';const bar = require(`${Atl}/x/${Btl}.js`)"
   const { code } = await requireSupport().transform(source)
 
   expect(code).toMatchInlineSnapshot(`
-    "const Atl = 'a';
+    "import hash_a/x/b.js from \\"a/x/b.js\\";
+    const Atl = 'a';
     const Btl = 'b';
-    const bar = require(\`\${Atl}/x/\${Btl}\`);"
+    const bar = hash_a / x / b.js;"
   `)
 })
 
