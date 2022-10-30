8b0b3585680e51cac22b8f53eb10ecc6304f57cc
chenjiezi <892774940@qq.com>
2022-10-30 18:11:25 +0800

    feat: "handleBinaryExporession" tool function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,19 @@
+import * as t from '@babel/types'
 import type * as myType from './types'
+
+export const handleBinaryExpression = (argument: t.BinaryExpression, declarationVariable: myType.Obj): string => {
+  const left = argument.left as myType.Leaf
+  const right = argument.right as myType.Leaf
+  const dfs = (leaf: myType.Leaf): string => {
+    if (t.isStringLiteral(leaf))
+      return leaf.value
+    else if (t.isIdentifier(leaf))
+      return declarationVariable[leaf.name]
+
+    return dfs(leaf.left as myType.Leaf) + dfs(leaf.right as myType.Leaf)
+  }
+  return dfs(left) + dfs(right)
+}
 
 export const isRawMethodCheck = (methodName: string): boolean => {
   return Object.prototype.toString.call([][methodName]).includes('Function')
