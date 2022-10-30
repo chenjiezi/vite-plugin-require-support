11746161ea3eef95693855a2763c85e4ebf97d35
chenjiezi <892774940@qq.com>
2022-10-30 11:50:43 +0800

    feat: "parseRequirePath" js tool function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,12 @@
+import type * as myType from './types'
+
 export const isRawMethodCheck = (methodName: string): boolean => {
   return Object.prototype.toString.call([][methodName]).includes('Function')
     || Object.prototype.toString.call({}[methodName]).includes('Function')
+}
+// TODO:
+export const parseRequirePath = (path: string): myType.PathElement => {
+  return {
+    moduleId: path,
+  }
 }
