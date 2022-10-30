19d2492948b1fdbd38012e3dcdbae6147f77a00a
chenjiezi <892774940@qq.com>
2022-10-30 14:53:11 +0800

    test: utils test

---
+++ test/utils.test.ts
@@ -0,0 +1,44 @@
+import { expect, test } from 'vitest'
+import { isRawMethodCheck, parseRequirePath } from '../src/utils'
+
+test('isRawMethodCheck', async () => {
+  const case1 = 'forEach'
+  const case2 = 'abc'
+  const case3 = 'toString'
+  expect(isRawMethodCheck(case1)).toMatchInlineSnapshot('true')
+  expect(isRawMethodCheck(case2)).toMatchInlineSnapshot('false')
+  expect(isRawMethodCheck(case3)).toMatchInlineSnapshot('true')
+})
+
+test('parseRequirePath - { moduleId }', async () => {
+  const path = 'module'
+  expect(parseRequirePath(path)).toMatchInlineSnapshot(`
+    Object {
+      "moduleId": "module",
+      "prefixPath": "",
+      "suffix": "",
+    }
+  `)
+})
+
+test('parseRequirePath - { moduleId, suffix }', async () => {
+  const path = 'module.js'
+  expect(parseRequirePath(path)).toMatchInlineSnapshot(`
+    Object {
+      "moduleId": "module",
+      "prefixPath": "",
+      "suffix": ".js",
+    }
+  `)
+})
+
+test('parseRequirePath - { prefixPath, moduleId, suffix }', async () => {
+  const path = './a/b/c/d.js'
+  expect(parseRequirePath(path)).toMatchInlineSnapshot(`
+    Object {
+      "moduleId": "d",
+      "prefixPath": "./a/b/c/",
+      "suffix": ".js",
+    }
+  `)
+})
