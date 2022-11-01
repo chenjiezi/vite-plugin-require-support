ec47a2c9a97f6b603bde00a87f96af80ba70e3ae
chenjiezi <892774940@qq.com>
2022-11-01 10:32:50 +0800

    test: string literal

--- test/requirePath.test.ts
+++ test/requirePath.test.ts
@@ -1,6 +1,16 @@
 import { expect, test } from 'vitest'
 import requireSupport from '../src/index'
 
+test('string literal', async () => {
+  const source = 'const bar = require(`a/x/b.js`)'
+  const { code } = await requireSupport().transform(source)
+
+  expect(code).toMatchInlineSnapshot(`
+    "import hash_b from \\"a/x/b.js\\";
+    const bar = hash_b;"
+  `)
+})
+
 test('template literal', async () => {
   // eslint-disable-next-line no-template-curly-in-string, @typescript-eslint/quotes
   const source = "const Atl = 'a';const Btl = 'b';const bar = require(`${Atl}/x/${Btl}.js`)"
